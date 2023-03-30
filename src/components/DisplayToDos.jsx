1041f358d9af8dc269cc4a57bb9add675bd0346e
NickBarkerDev <nickthedevguy@gmail.com>
2023-03-30 02:06:34 -0700

    added onChange handler for text input, createToDo for adding each item to the array, used map to iterate over array and display all todos.

--- src/components/DisplayToDos.jsx
+++ src/components/DisplayToDos.jsx
@@ -1,9 +1,26 @@
-
+import './DisplayToDos.css'
 
 const DisplayToDos = (props) => {
+
+  const {allToDos, setAllToDos} = props
+
   return (
-    <div>
+    <div className="container">
+      <h2>My ToDo List:</h2>
+      <div className="all-todos">
+        {
+          allToDos.map((item, index) => (
+            <div className="each-todo-container">
+              <div key={index} className="one-todo">
+                <h3 className="task-content">{item.content}</h3>
+                <span>Task Complete?</span><input type="checkbox" />
+              </div>  
+            </div>
+          ))
+        }
+      </div>
       
+
     </div>
   );
 }
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -9,23 +9,37 @@ const TodoForm = (props) => {
     isComplete: false
   })
 
-  // Destructure getter and setter for todo list
-  const { allToDos, setAllToDos } = props
+  // Destructure input state into individual variables
+  const {content, isComplete} = oneToDo
+
+  // Destructure getter and setter for full todo list
+  const { setAllToDos } = props
   
 
-
+  // change handler for text input
   const onChangeHandler = (e) => {
     setOneToDo(prevState => {return {...prevState, [e.target.name]: e.target.value}})
+  }
+
+  // submit handler for submitting a task
+  const createToDo = (e) => {
+    e.preventDefault()
+    setAllToDos(prevState => {return [...prevState, oneToDo]})
+
+    setOneToDo({
+      content: '',
+      isComplete: false
+    })
   }
 
   
   return (
     <div className='todo-form'>
       <h1>To Do List</h1>
-      <form action="submit">
+      <form action="submit" onSubmit={createToDo}>
         <div className="form-group">
           <label htmlFor="add-task">Add a task to your ToDo List:</label>
-          <input type="text" className="form-control" name="content" onChange={onChangeHandler} />
+          <input type="text" className="form-control" name="content" onChange={onChangeHandler} value={content} />
           <input type="submit" className="form-control btn btn-primary" value="Add Task" />
         </div>
       </form>
