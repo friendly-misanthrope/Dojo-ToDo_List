bba8a2df2725d0c56892e72392109c729ed6725d
NickBarkerDev <nickthedevguy@gmail.com>
2023-03-30 02:46:54 -0700

    Added handler to toggle isComplete and display tasks accordingly. Added Comments.

--- src/components/DisplayToDos.jsx
+++ src/components/DisplayToDos.jsx
@@ -4,24 +4,44 @@ const DisplayToDos = (props) => {
 
   const {allToDos, setAllToDos} = props
 
+  const toggleComplete = (todo) => {
+    const updatedTaskList = allToDos.map((thisTodo) => {
+      if (thisTodo === todo) {
+        thisTodo.isComplete = !thisTodo.isComplete
+      }
+      return thisTodo
+    })
+    setAllToDos(updatedTaskList)
+  }
+
   return (
     <div className="container">
-      <h2>My ToDo List:</h2>
+      {
+        allToDos.length > 0 ?
+        <h2>My ToDo List:</h2>
+        : null
+      }
+      
       <div className="all-todos">
         {
           allToDos.map((item, index) => (
-            <div className="each-todo-container">
-              <div key={index} className="one-todo">
-                <h3 className="task-content">{item.content}</h3>
-                <span>Task Complete?</span><input type="checkbox" />
+            <div key={index} className="each-todo-container">
+              <div className="one-todo">
+                {
+                  item.isComplete ?
+                  <h3 className="task-content is-complete">{item.content}</h3>
+                  : <h3 className="task-content">{item.content}</h3>
+                }
+                <div className="complete">
+                  <span>Task Complete?</span>
+                  <input type="checkbox" checked={item.isComplete} onClick={() => toggleComplete(item)} />
+                </div>
                 <button className="btn btn-danger">Delete</button>
               </div>  
             </div>
           ))
         }
       </div>
-      
-
     </div>
   );
 }
