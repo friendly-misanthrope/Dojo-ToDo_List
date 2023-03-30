574004d0f9cb16ab7faecf6d2d7b2f1226fdc0c5
NickBarkerDev <nickthedevguy@gmail.com>
2023-03-30 01:10:49 -0700

    added useState for value in text input and an onChange handler for that state

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,8 +1,23 @@
 import './TodoForm.css'
+import { useState } from 'react'
 
 const TodoForm = (props) => {
+  
+  // Set up state for values in input
+  const [oneToDo, setOneToDo] = useState({
+    content: '',
+    isComplete: false
+  })
 
+  // Destructure getter and setter for todo list
   const { allToDos, setAllToDos } = props
+  
+
+
+  const onChangeHandler = (e) => {
+    setOneToDo(prevState => {return {...prevState, [e.target.name]: e.target.value}})
+  }
+
   
   return (
     <div className='todo-form'>
@@ -10,7 +25,7 @@ const TodoForm = (props) => {
       <form action="submit">
         <div className="form-group">
           <label htmlFor="add-task">Add a task to your ToDo List:</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" name="content" onChange={onChangeHandler} />
           <input type="submit" className="form-control btn btn-primary" value="Add Task" />
         </div>
       </form>
